029f1defa8830cd21a59609dcf925f757645b927
Mayank K <mayankgowda83@gamil.com>
2025-03-10 17:24:10 +0530

    PES2UG22CS308

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and done the lab  \n";
+  cout << "I have successfully built and done the CC lab  \n";
   return 0;
 }
